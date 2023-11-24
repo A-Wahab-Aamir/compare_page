1e6c651d4e85c2ce510926280680769f894272d6
abdul wahab amir <abdulwahabamir4@gmail.com>
2023-11-24 12:40:57 +0500

    first commit

---
+++ file.js
@@ -0,0 +1,59 @@
+var  product= [
+    {
+           "name": "",
+           "detail": "",
+           "price": "",
+           "description": "",
+           "brand": "",
+           "image": "images/0.png"
+       },
+       {
+           "name": "iPhone 13",
+           "detail": "The latest iPhone from Apple",
+           "price": "12000",
+           "description": "Snapdragon 888 (USA)/Exynos 2100 (Global)",
+           "brand": "I-Phone",
+           "image": "images/1.jpg"
+       },
+       {
+           "name": "Samsung Galaxy S21",
+           "detail": "Flagship smartphone from Samsung",
+           "price": "11000",
+           "description": "Snapdragon 888 (USA)/Exynos 2100 (Global)",
+           "brand": "Samsung",
+           "image": "images/2.jpg"
+       },
+       {
+           "name": "OnePlus 9 Pro",
+           "detail": "Flagship killer from OnePlus",
+           "price": "10000",
+           "description": "Snapdragon 888",
+           "brand": "OnePlus",
+           "image": "images/3.jpg"
+       },
+       {
+           "name": "Google Pixel 6",
+           "detail": "Flagship from Google with advanced camera capabilities",
+           "price": "9000",
+           "description": "Google Tensor",
+           "brand": "Google",
+           "image": "images/4.jpg"
+       },
+       {
+           "name": "Xiaomi Mi 11",
+           "detail": "Feature-packed flagship from Xiaomi",
+           "price": "8000",
+           "description": "Snapdragon 888",
+           "brand": "Xiaomi",
+           "image": "images/5.jpg"
+       },
+       {
+        "name": "Nokia 3310",
+        "detail": "Feature-packed flagship from Xiaomi",
+        "price": "8000",
+        "description": "Snapdragon 888",
+        "brand": "Xiaomi",
+        "image": "images/6.jpg"
+    }
+
+   ]
